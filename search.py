69772060b6600e7bd4aa8abfe9d47992c3d1d35b
abhayrm20 <abhayrm20@gmail.com>
2020-03-01 08:10:01 +0530

    Better push

--- search.py
+++ search.py
@@ -7,8 +7,6 @@ def search(dir,fname):
         for files in glob.glob( "*.*" ):
             f = open( files, 'r' )
             file_contents = f.read()
-            #if "AIzaSyAbg4XZm-68VmFtLnKWSAnNanO1oNTukT4" in file_contents:
-            #if "AIzaSyDkM2fJKHovUnCx5AihUbQc136XgGutASc" in file_contents:
             if fname in file_contents:
                     print
                     print "*********************************************************************************"
@@ -27,4 +25,5 @@ search(dir,fname)
 for x in os.walk(dir):
     dir = x[0]
     #print "Iterating dir: ", dir
-    search(dir,fname)
+    if('env' not in dir and 'pycache' not in dir):
+        search(dir,fname)
